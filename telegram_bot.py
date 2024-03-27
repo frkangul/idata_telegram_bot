2bad036358fb687e0f84b0909a267247f72cb686
Furkan Gul <frkangul@gmail.com>
2024-03-27 14:19:52 +0300

    update: add logger

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,22 +1,29 @@
 import asyncio
-
+import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 from find_idata_randevu import find_randevu
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="iData dan randevu bulacak botum. /randevu_ara komutu ile istanbul içinde standart hizmet için 2 kişilik radevu arar. Uygun randevu bulunca mesaj atar. İstanbul içinde prime hizmet için randevu almak istiyorsanız /prime_radevu_ara kullan.")
 
 async def randevu_ara(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logging.info("Randevu aranmaya baslandi!")
     while True:
         location, code = find_randevu()
         if code == 1:
-            await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{location} da randevu bulundu!")
+            logging.info(f"{location} da randevu bulundu! Bot aramayi durdurdu.")
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{location} da randevu bulundu! Bot aramayi durdurdu.")
             break  # Exit the loop if a valid randevu is found
         elif code == -1:
-            await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{location} da randevu bulunmuş olabilir!")
+            logging.info(f"{location} da randevu bulunmuş olabilir! Bot aramaya devam ediyor.")
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{location} da randevu bulunmuş olabilir! Bot aramaya devam ediyor.")
             await asyncio.sleep(60)  # Wait for 60 seconds before retrying
 
 async def prime_radevu_ara(update: Update, context: ContextTypes.DEFAULT_TYPE):
